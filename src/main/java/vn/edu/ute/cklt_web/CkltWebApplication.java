cad338d05502b22a17d25c86b9b0eab869a94d70
Haquoctoan2006 <24162130@student.hcmute.edu.vn>
2026-09-13 22:11:53 +0700

    khoi tao project Spring Boot voi JSP, JWT, Sitemesh, Cloudinary

---
+++ src/main/java/vn/edu/ute/cklt_web/CkltWebApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.ute.cklt_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkltWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkltWebApplication.class, args);
+	}
+
+}
